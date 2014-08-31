79009cdeebbc384b9712b247fd6e2f1d3b485499
Gijs W <thisisnimrr@gmail.com>
2014-08-31 11:59:00 +0200

    Update i9300_official.sh

--- i9300_official.sh
+++ i9300_official.sh
@@ -36,17 +36,10 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 fi
  
 # Notify user of success 
-beep -f 784 -r 3 -l 100
-sleep .1
-beep -f 784 -l 600
-beep -f 622 -l 600
-beep -f 698 -l 600
-beep -f 784 -l 200
-sleep .2
-beep -f 698 -l 200
-beep -f 784 -l 800
+echo "Your build completed succesfully, Dirty Unicorns kicks ass!"
+say "Your build completed succesfully, Dirty Unicorns kicks ass!"
 
-echo 'Build completed successfully!'
+ctrl+c
 
 ############################################################
 # CLOBBER                			                       #
@@ -59,3 +52,4 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     . ~/du/clobber.sh    
 fi
+
